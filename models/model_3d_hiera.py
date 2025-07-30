44602ca673a235ac1b7b2eab8f85d11d3b9cad27
Zhengrong Yi <44876183+Zhengro@users.noreply.github.com>
2025-07-30 12:50:42 +0800

    Finetune hiera

---
+++ models/model_3d_hiera.py
@@ -0,0 +1,135 @@
+# Hiera for 3D image classification
+
+# References:
+# hiera: https://github.com/facebookresearch/hiera
+
+from functools import partial
+from pathlib import Path
+
+from hiera import Head, Hiera, MaskedAutoencoderHiera
+import torch
+from torch import nn
+
+try:
+    from experiment_config import config
+except ImportError:
+    import sys
+
+    project_root = str(Path(__file__).resolve().parent.parent)
+    if project_root not in sys.path:
+        sys.path.append(project_root)
+
+    from experiment_config import config
+
+from .model_utils import interpolate_embeddings_spatial, interpolate_embeddings_temporal, get_pretrained_model
+
+
+class HieraFusionHead(MaskedAutoencoderHiera):
+    """
+    This model inherits the encoder and multi-scale fusion from MaskedAutoencoderHiera
+    but replaces the MAE decoder with a classification head.
+    """
+
+    def __init__(self, num_classes, head_dropout, head_init_scale, sep_pos_embed, **kwargs):
+        super().__init__(num_classes=num_classes, head_dropout=head_dropout,
+                         head_init_scale=head_init_scale, sep_pos_embed=sep_pos_embed, **kwargs)
+
+        # remove all the decoder-specific parts
+        del self.decoder_embed
+        del self.mask_token
+        del self.decoder_pos_embed
+        del self.decoder_blocks
+        del self.decoder_norm
+        del self.decoder_pred
+
+        # add the classification-specific layers
+        encoder_dim_out = self.encoder_norm.normalized_shape[0]
+        self.norm = nn.LayerNorm(encoder_dim_out, eps=1e-6)
+        self.head = Head(encoder_dim_out, num_classes,
+                         dropout_rate=head_dropout)
+
+        # initialize everything (as done in Hiera)
+        if sep_pos_embed:
+            nn.init.trunc_normal_(self.pos_embed_spatial, std=0.02)
+            nn.init.trunc_normal_(self.pos_embed_temporal, std=0.02)
+        else:
+            nn.init.trunc_normal_(self.pos_embed, std=0.02)
+        self.apply(partial(self._init_weights))
+        self.head.projection.weight.data.mul_(head_init_scale)
+        self.head.projection.bias.data.mul_(head_init_scale)
+
+    def forward(self, x):
+        x, _ = self.forward_encoder(x, mask_ratio=0.0)
+        # flatten all dimensions except the first (batch) and the last (channels)
+        x = x.flatten(start_dim=1, end_dim=-2)
+        x = x.mean(dim=1)
+        x = self.norm(x)
+        x = self.head(x)
+        return x
+
+
+class Hiera3D(nn.Module):
+    def __init__(self, image_size, image_depth, kind="pretrained", num_classes=1, patch_size=4, patch_depth=2):
+        super().__init__()
+        assert kind in ["pretrained",
+                        "finetuned"], f"Unknown model kind: {kind}"
+        model = HieraFusionHead if config.FUSION_HEAD_ENABLED else Hiera
+        self.hiera = model(
+            embed_dim=config.PRETRAINED_MODEL_CONFIGS[config.PRETRAINED_MODEL]["embed_dim"],
+            num_heads=config.PRETRAINED_MODEL_CONFIGS[config.PRETRAINED_MODEL]["num_heads"],
+            stages=config.PRETRAINED_MODEL_CONFIGS[config.PRETRAINED_MODEL]["stages"],
+            num_classes=num_classes,
+            input_size=(image_depth, image_size, image_size),
+            q_stride=(1, 2, 2),
+            mask_unit_size=(1, 8, 8),
+            patch_kernel=(3, 7, 7),
+            patch_stride=(2, 4, 4),
+            patch_padding=(1, 3, 3),
+            sep_pos_embed=True,
+            drop_path_rate=config.DROP_PATH_RATE,
+            head_dropout=config.HEAD_DROPOUT,
+            head_init_scale=config.HEAD_INIT_SCALE,
+            q_pool=2 if config.FUSION_HEAD_ENABLED else 3
+        )
+        # disable softmax in head for eval and testing
+        self.hiera.head.act_func = nn.Identity()
+
+        if kind == "pretrained":
+            state_dict = get_pretrained_model()
+            if "model_state" in state_dict:
+                state_dict = state_dict["model_state"]
+
+            new_state_dict = interpolate_embeddings_spatial(
+                image_size=image_size,
+                patch_size=patch_size,
+                model_state=state_dict
+            )
+            new_state_dict = interpolate_embeddings_temporal(
+                image_depth=image_depth,
+                patch_depth=patch_depth,
+                model_state=new_state_dict
+            )
+            if "head.projection.weight" in new_state_dict and \
+                    num_classes != new_state_dict["head.projection.weight"].shape[0]:
+                del new_state_dict["head.projection.weight"]
+                del new_state_dict["head.projection.bias"]
+
+            self.hiera.load_state_dict(new_state_dict, strict=False)
+
+    def forward(self, x):
+        return self.hiera(x)
+
+
+if __name__ == "__main__":
+
+    IMG_SIZE, IMG_DEPTH = 64, 16
+    model = Hiera3D(image_size=IMG_SIZE, image_depth=IMG_DEPTH)
+    print(
+        f"Output shape: {model(torch.randn(1, 3, IMG_DEPTH, IMG_SIZE, IMG_SIZE)).shape}")
+    print(
+        f"Number of parameters: {sum(p.numel() for p in model.parameters())}")
+    for name, param in model.named_parameters():
+        if param.requires_grad:
+            print(f"{name}: {param.size()}")
+        else:
+            print(f"[Frozen] {name}: {param.size()}")
---
+++ models/model_utils.py
@@ -0,0 +1,191 @@
+# Utility functions for model handling in Hiera3D
+
+# Portions of this code are adapted from PyTorch TorchVision (Copyright (c) Soumith Chintala 2016, all rights reserved).
+# Source: https://github.com/pytorch/vision
+# License: BSD 3-Clause License (see https://github.com/pytorch/vision/blob/main/LICENSE)
+
+from collections import OrderedDict
+import math
+from pathlib import Path
+import urllib.request
+
+import torch
+from torch import nn
+from tqdm import tqdm
+
+try:
+    from experiment_config import config
+except ImportError:
+    import sys
+
+    project_root = str(Path(__file__).resolve().parent.parent)
+    if project_root not in sys.path:
+        sys.path.append(project_root)
+
+    from experiment_config import config
+
+
+def interpolate_embeddings_spatial(
+    image_size: int,
+    patch_size: int,
+    model_state: "OrderedDict[str, torch.Tensor]",
+    pos_embedding_key: str = "pos_embed_spatial",
+    interpolation_mode: str = "bicubic"
+) -> "OrderedDict[str, torch.Tensor]":
+    """
+    Interpolate positional embeddings for pretrained models.
+
+    This is particularly useful when loading a checkpoint and applying the model
+    to images with a different input resolution than the one it was trained on.
+
+    Adapted from:
+    https://github.com/pytorch/vision/blob/main/torchvision/models/vision_transformer.py
+
+    Args:
+        image_size (int): Image size of the new model.
+        patch_size (int): Patch size of the new model.
+        model_state (OrderedDict[str, torch.Tensor]): State dict of the pretrained model.
+        pos_embedding_key (str): Key of the position embedding in the state dict.
+                                 Default: pos_embed_spatial.
+        interpolation_mode (str): The algorithm used for interpolation. Default: bicubic.
+
+    Returns:
+        OrderedDict[str, torch.Tensor]: A state dict which can be loaded into the new model.
+    """
+    # Shape of pos_embedding is (1, seq_length, hidden_dim)
+    pos_embedding = model_state[pos_embedding_key]
+    n, seq_length, hidden_dim = pos_embedding.shape
+    if n != 1:
+        raise ValueError(
+            f"Unexpected position embedding shape: {pos_embedding.shape}")
+
+    new_seq_length = (image_size // patch_size) ** 2
+
+    # Need to interpolate the weights for the position embedding.
+    # We do this by reshaping the positions embeddings to a 2d grid, performing
+    # an interpolation in the (h, w) space and then reshaping back to a 1d grid.
+    if new_seq_length != seq_length:
+        # (1, seq_length, hidden_dim) -> (1, hidden_dim, seq_length)
+        pos_embedding = pos_embedding.permute(0, 2, 1)
+        seq_length_1d = int(math.sqrt(seq_length))
+        if seq_length_1d * seq_length_1d != seq_length:
+            raise ValueError(
+                f"seq_length is not a perfect square! Instead got \
+                    seq_length_1d * seq_length_1d = \
+                        {seq_length_1d * seq_length_1d} and seq_length = {seq_length}"
+            )
+
+        # (1, hidden_dim, seq_length) -> (1, hidden_dim, seq_l_1d, seq_l_1d)
+        pos_embedding = pos_embedding.reshape(
+            1, hidden_dim, seq_length_1d, seq_length_1d)
+        new_seq_length_1d = image_size // patch_size
+
+        # Perform interpolation.
+        # (1, hidden_dim, seq_l_1d, seq_l_1d) -> (1, hidden_dim, new_seq_l_1d, new_seq_l_1d)
+        new_pos_embedding = nn.functional.interpolate(
+            pos_embedding,
+            size=new_seq_length_1d,
+            mode=interpolation_mode,
+            align_corners=True
+        )
+
+        # (1, hidden_dim, new_seq_l_1d, new_seq_l_1d) -> (1, hidden_dim, new_seq_length)
+        new_pos_embedding = new_pos_embedding.reshape(
+            1, hidden_dim, new_seq_length)
+
+        # (1, hidden_dim, new_seq_length) -> (1, new_seq_length, hidden_dim)
+        new_pos_embedding = new_pos_embedding.permute(0, 2, 1)
+
+        model_state[pos_embedding_key] = new_pos_embedding
+
+    return model_state
+
+
+def interpolate_embeddings_temporal(
+    image_depth: int,
+    patch_depth: int,
+    model_state: "OrderedDict[str, torch.Tensor]",
+    pos_embedding_key: str = "pos_embed_temporal",
+    interpolation_mode: str = "linear"
+) -> "OrderedDict[str, torch.Tensor]":
+    """
+    Interpolate positional embeddings for the temporal dimension during checkpoint loading.
+
+    Similar to `interpolate_embeddings_spatial`, this function enables applying
+    pretrained models to volumes with different depths (temporal resolutions).
+
+    Args:
+        image_depth (int): Image depth of the new model.
+        patch_depth (int): Patch depth of the new model.
+        model_state (OrderedDict[str, torch.Tensor]): State dict of the pretrained model.
+        pos_embedding_key (str): Key of the position embedding in the state dict.
+                                 Default: pos_embed_temporal.
+        interpolation_mode (str): The algorithm used for interpolation. Default: linear.
+
+    Returns:
+        OrderedDict[str, torch.Tensor]: A state dict which can be loaded into the new model.
+    """
+    # Shape of pos_embedding is (1, seq_length, hidden_dim)
+    pos_embedding = model_state[pos_embedding_key]
+    n, seq_length, hidden_dim = pos_embedding.shape
+    if n != 1:
+        raise ValueError(
+            f"Unexpected position embedding shape: {pos_embedding.shape}")
+
+    new_seq_length = image_depth // patch_depth
+
+    # Need to interpolate the weights for the position embedding.
+    if new_seq_length != seq_length:
+        # (1, seq_length, hidden_dim) -> (1, hidden_dim, seq_length)
+        pos_embedding = pos_embedding.permute(0, 2, 1)
+
+        # Perform interpolation.
+        # (1, hidden_dim, seq_length) -> (1, hidden_dim, new_seq_length)
+        new_pos_embedding = nn.functional.interpolate(
+            pos_embedding,
+            size=new_seq_length,
+            mode=interpolation_mode,
+            align_corners=True
+        )
+
+        # (1, hidden_dim, new_seq_length) -> (1, new_seq_length, hidden_dim)
+        new_pos_embedding = new_pos_embedding.permute(0, 2, 1)
+
+        model_state[pos_embedding_key] = new_pos_embedding
+
+    return model_state
+
+
+def download_file_with_progress(url, destination_path: Path):
+    """
+    Download a file from a URL to a destination Path with a progress bar.
+    """
+    print(f"Downloading from {url} to {destination_path}...")
+    with urllib.request.urlopen(url) as response, destination_path.open("wb") as out_file:
+        total_size = int(response.headers.get("content-length", 0))
+        block_size = 8192  # 8KB chunks
+        with tqdm(total=total_size, unit="B", unit_scale=True, desc="Downloading", leave=True) as pbar:
+            for chunk in iter(lambda: response.read(block_size), b""):
+                out_file.write(chunk)
+                pbar.update(len(chunk))
+    print("Download complete!")
+
+
+def get_pretrained_model():
+    """
+    Get the state dictionary for the pretrained model.
+    """
+    config.RESOURCES.mkdir(parents=True, exist_ok=True)
+    pretrained_model_url = config.PRETRAINED_MODEL_CONFIGS[config.PRETRAINED_MODEL]["mae_k400"]
+    local_pretrained_model_path = config.RESOURCES / \
+        Path(pretrained_model_url).name
+    if not local_pretrained_model_path.exists():
+        print(
+            f"Local pretrained model not found at {local_pretrained_model_path}")
+        download_file_with_progress(
+            pretrained_model_url, local_pretrained_model_path)
+    else:
+        print(
+            f"Local pretrained model found at {local_pretrained_model_path}. Loading directly...")
+    state_dict = torch.load(local_pretrained_model_path, map_location="cpu")
+    return state_dict
